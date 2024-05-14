f85c0a29f955c1fb9b95dd521b891bc2efe595e1
chlgkdms <haeun3882@gmail.com>
2024-05-14 12:26:27 +0900

    feat :: font

---
+++ app/src/main/java/com/example/check/ui/theme/Typography.kt
@@ -0,0 +1,12 @@
+package com.example.check.ui.theme
+
+import androidx.compose.runtime.Immutable
+import java.time.format.TextStyle
+
+@Immutable
+class Typography  internal constructor(
+    val body1: TextStyle,
+    val body2: TextStyle,
+    val heading1: TextStyle,
+){
+}
