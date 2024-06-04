7abc187631d6c796638e6b0494f99931e93c755f
chlgkdms <haeun3882@gmail.com>
2024-06-04 11:56:44 +0900

    feat :: saved, my checklist screen

---
+++ app/src/main/java/com/example/check/feature/mychecklist/MyChecklistScreen.kt
@@ -0,0 +1,8 @@
+package com.example.check.feature.mychecklist
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun MyChecklistScreen() {
+
+}
---
+++ app/src/main/java/com/example/check/feature/saved/SavedChecklistScreen.kt
@@ -0,0 +1,8 @@
+package com.example.check.feature.saved
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun SavedChecklistScreen() {
+
+}
