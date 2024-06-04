6c4498441b16dfc902f2de128e9137684660e697
chlgkdms <haeun3882@gmail.com>
2024-06-04 11:56:51 +0900

    feat :: subject detail

---
+++ app/src/main/java/com/example/check/feature/subjectdetail/SubjectDetail.kt
@@ -0,0 +1,9 @@
+package com.example.check.feature.subjectdetail
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+internal fun SubjectDetail(navController: NavController) {
+
+}
