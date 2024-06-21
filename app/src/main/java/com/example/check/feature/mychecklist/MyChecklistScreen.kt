1b9db2dea5055d0e80b1d41e10474a2e67dfd151
chlgkdms <haeun3882@gmail.com>
2024-06-21 09:27:41 +0900

    feat :: 디자인 수정

--- app/src/main/java/com/example/check/feature/mychecklist/MyChecklistScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.check.feature.mychecklist
-
-import androidx.compose.runtime.Composable
-
-@Composable
-internal fun MyChecklistScreen() {
-
-}
--- app/src/main/java/com/example/check/feature/saved/SavedChecklistScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.check.feature.saved
-
-import androidx.compose.runtime.Composable
-
-@Composable
-internal fun SavedChecklistScreen() {
-
-}
