62bae2d1f38dac87b0563c03fb705720a4635782
chlgkdms <haeun3882@gmail.com>
2024-06-21 09:29:33 +0900

    feat :: android entry point

--- app/src/main/java/com/example/check/MainActivity.kt
+++ app/src/main/java/com/example/check/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.check.navigation.CheckApp
 import com.example.check.designsystem.theme.CheckTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
