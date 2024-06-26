f08687bf199ab5d820de248916975c44d719ce76
chlgkdms <haeun3882@gmail.com>
2024-06-26 22:01:24 +0900

    feat :: request url

--- app/src/main/java/com/example/check/domain/util/RequestUrl.kt
+++ app/src/main/java/com/example/check/domain/util/RequestUrl.kt
@@ -5,11 +5,15 @@ sealed interface RequestUrl {
         const val auth = "/user"
         const val signIn = "$auth/login"
         const val signUp = "$auth/signup"
+        const val profile = "$auth/profile"
     }
 
     data object Subject : RequestUrl {
         const val subject = "/subjects"
         const val fetchSubject = "$subject/all"
         const val fetchSubjectDetail = "$subject/{subject-id}"
+        const val createSubject = "$subject/create"
+        const val updateSaved = "/checklists/update/{checklist-id}"
+        const val createChecklist = "/checklistcontents/create/{subject-id}"
     }
 }
